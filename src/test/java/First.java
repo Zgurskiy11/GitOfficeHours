e22186083d7087880ce42154db7a45d7161cb7fc
Zgurskiy11 <zgurskiy11@gmail.com>
2021-05-24 16:25:18 -0600

    second commit,, made changes to both files

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -2,4 +2,6 @@ public class First {
 
     // this is the first class
     //will publish after
+
+    //iuguygjgjhgjhgj one line added
 }
---
+++ src/test/java/Second.java
@@ -0,0 +1,9 @@
+public class Second {
+
+
+    /*
+    2nd file, added some things
+    remove in line from first after
+    bla bla bla
+     */
+}
